852d6222d8b9e78d0cca8ca6e2239ff9a2a1ad51
putypuruty <putypuruty@bc0df05e-133d-0410-bb72-81d3b1e9bcfd>
2008-09-20 18:17:02 +0000

    refactored: defined constants, removed unused variable
    git-svn-id: http://jobberbase.googlecode.com/svn/trunk@70 bc0df05e-133d-0410-bb72-81d3b1e9bcfd

--- app/page_home.php
+++ app/page_home.php
@@ -1,13 +1,14 @@
 <?php
+	define('NUMBER_OF_MOST_APPLIED_TO_JOBS_TO_GET', 7);
+	define('NUMBER_OF_LATEST_JOBS_TO_GET', 7);
+	
 	// get jobs
 	$smarty->assign('jobs_count_all', $job->CountJobs());
 	$smarty->assign('jobs_count_all_categs', $job->GetJobsCountForAllCategs());
 	
-	$smarty->assign('most_applied_to_jobs', $job->GetMostAppliedToJobs(7));
+	$smarty->assign('most_applied_to_jobs', $job->GetMostAppliedToJobs(NUMBER_OF_MOST_APPLIED_TO_JOBS_TO_GET));
 	
-	$smarty->assign('jobs_count', $job->CountJobs());
-	
-	$smarty->assign('latest_jobs', $job->GetJobs(0, 0, 7, 0, 0));
+	$smarty->assign('latest_jobs', $job->GetJobs(0, 0, NUMBER_OF_LATEST_JOBS_TO_GET, 0, 0));
 
 	$smarty->assign('current_category', 'home');
 	
