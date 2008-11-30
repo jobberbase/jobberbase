b2862a1d9bf206679d22b4c03a344bac85cc03d4
putypuruty@gmail.com <putypuruty@gmail.com@bc0df05e-133d-0410-bb72-81d3b1e9bcfd>
2008-11-30 18:01:05 +0000

    refactored code changed: call CheckPosterEmail because it's no longer called when job->GetInfo() is called
    git-svn-id: http://jobberbase.googlecode.com/svn/trunk@94 bc0df05e-133d-0410-bb72-81d3b1e9bcfd

--- app/page_publish.php
+++ app/page_publish.php
@@ -2,28 +2,35 @@
 	$job = new Job($id);
 
 	// send notification to admin
-	$nicu = new Postman();
-	$nicu->MailPublishToAdmin($job->GetInfo());
+	$postMan = new Postman();
+	
+	$jobInfo = $job->GetInfo();
+	$jobInfo['check_poster_email'] = $job->CheckPosterEmail();
+	
+	$postMan->MailPublishToAdmin($jobInfo);
 
 	// send notification to user
-	if ($job->CheckPosterEmail())
+	if ($jobInfo['check_poster_email'])
 	{
 		// post published
-		$nicu->MailPublishToUser($job->GetInfo());	
+		$postMan->MailPublishToUser($jobInfo);	
 		$html_title = $translations['jobs']['publish_success'] . ' / ' . SITE_NAME;
 		$smarty->assign('first_time_post', 0);
 	}
 	else
 	{
 		// post in pending status
-		$nicu->MailPublishPendingToUser($job->mPosterEmail);	
+		$postMan->MailPublishPendingToUser($job->mPosterEmail);	
 		$html_title = $translations['jobs']['add_success'] . ' / ' . SITE_NAME;
 		$smarty->assign('first_time_post', 1);
-	}	
+	}
+	
 	$job->Publish();
 	
 	$job_title = BASE_URL . 'job/' . $job->mId . '/' . $job->mUrlTitle . '/';
+	
 	$smarty->assign('auth', $job->GetAuth());
 	$smarty->assign('job_url', $job_title);
+	
 	$template = 'publish-confirmation.tpl';
 ?>
