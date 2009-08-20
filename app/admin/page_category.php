270dd33c7f8a815db896a17edf5fa036337216fe
putypuruty@gmail.com <putypuruty@gmail.com@bc0df05e-133d-0410-bb72-81d3b1e9bcfd>
2009-08-20 18:09:02 +0000

    code cleanup
    git-svn-id: http://jobberbase.googlecode.com/svn/trunk@215 bc0df05e-133d-0410-bb72-81d3b1e9bcfd

--- app/admin/page_category.php
+++ app/admin/page_category.php
@@ -1,26 +1,6 @@
 <?php
 	$type_id = get_type_id_by_varname($extra);
 	$city_id = false;
-	/*if ($extra == 'full-time')
-	{
-		$type_id = JOBTYPE_FULLTIME;
-		$city_id = false;
-	}
-	else if ($extra == 'part-time')
-	{
-		$type_id = JOBTYPE_PARTTIME;
-		$city_id = false;
-	}
-	else if ($extra == 'freelance')
-	{
-		$type_id = JOBTYPE_FREELANCE;
-		$city_id = false;
-	}
-	else
-	{
-		$type_id = false;
-		$city_id = false;
-	}*/
 	
 	if($type_id && $id != 'all')
 	{
@@ -34,8 +14,8 @@
 			redirect_to(BASE_URL . 'page-unavailable/');
 			exit;
 		}
-		
 	}
+	
 	if (!$type_id && $id != 'all')
 	{
 		if ($job->IsValidCategory($id))
@@ -60,8 +40,6 @@
 	
 	$firstLimit = $paginator->getFirstLimit();
 	$lastLimit = $paginator->getLastLimit();
-	
-	
 	
 	$the_jobs = array();
 	$the_jobs = $job->GetJobsPaginate(0, $id, $firstLimit, JOBS_PER_PAGE, 0, 0, false, $city_id, $type_id);
