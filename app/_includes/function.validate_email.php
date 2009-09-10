f37760f94738fe137ef5f4f88f4bac5f61988ec3
navjotjsingh@gmail.com <navjotjsingh@gmail.com@bc0df05e-133d-0410-bb72-81d3b1e9bcfd>
2009-09-10 06:48:44 +0000

    eregi is deprecated.
    git-svn-id: http://jobberbase.googlecode.com/svn/trunk@289 bc0df05e-133d-0410-bb72-81d3b1e9bcfd

--- app/_includes/function.validate_email.php
+++ app/_includes/function.validate_email.php
@@ -13,7 +13,7 @@
 
 function validate_email($string) 
 {
-	if(eregi("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[_a-z0-9-]+)*(\.[a-z]{2,3})$", $string))
+	if(preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[_a-z0-9-]+)*(\.[a-z]{2,3})$/i", $string))
 	{
     return true;
 	}
