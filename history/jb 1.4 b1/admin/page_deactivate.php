45a6734a17b40348d09b5aaf4d34a3d055efa970
navjotjsingh@gmail.com <navjotjsingh@gmail.com@bc0df05e-133d-0410-bb72-81d3b1e9bcfd>
2009-09-16 09:00:30 +0000

    Cleaning up trunk/history - Use Tags Instead.
    git-svn-id: http://jobberbase.googlecode.com/svn/trunk@329 bc0df05e-133d-0410-bb72-81d3b1e9bcfd

--- history/jb 1.4 b1/admin/page_deactivate.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$j = new Job($_POST['job_id']);
-	$deactivator = $j->Deactivate();
-	echo 1;
-	exit;
-?>
--- history/jb 1.4 b1/admin/page_delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$j = new Job($_POST['job_id']);
-	if($j->DeleteJobAdmin())
-		echo 1;
-	else
-		echo "0";
-	exit;
-?>
