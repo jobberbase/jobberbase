62c966c9d57f2f078c6709281647e7ce7de25d58
putypuruty@gmail.com <putypuruty@gmail.com@bc0df05e-133d-0410-bb72-81d3b1e9bcfd>
2009-09-20 14:37:11 +0000

    fixed: replaced htmlentities with htmlspecialchars because we only want to escape the chars that have special meaning in html
    git-svn-id: http://jobberbase.googlecode.com/svn/trunk@349 bc0df05e-133d-0410-bb72-81d3b1e9bcfd

--- public/page_home.php
+++ public/page_home.php
@@ -27,9 +27,9 @@
 
 	$smarty->assign('current_category', 'home');
 
-	$smarty->assign('seo_title', htmlentities($settings['html_title']));
-	$smarty->assign('seo_desc', htmlentities($settings['meta_description']));
-	$smarty->assign('seo_keys', htmlentities($settings['meta_keywords']));
+	$smarty->assign('seo_title', htmlspecialchars($settings['html_title']));
+	$smarty->assign('seo_desc', htmlspecialchars($settings['meta_description']));
+	$smarty->assign('seo_keys', htmlspecialchars($settings['meta_keywords']));
 	
 	$template = 'index.tpl';
 ?>
