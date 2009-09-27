e19ad3e9e114b2657339939f2cb81a670a28854f
putypuruty@gmail.com <putypuruty@gmail.com@bc0df05e-133d-0410-bb72-81d3b1e9bcfd>
2009-09-27 16:16:19 +0000

    + refactored the code that is called when clicking on a job category => easier to read and understand
    git-svn-id: http://jobberbase.googlecode.com/svn/trunk@357 bc0df05e-133d-0410-bb72-81d3b1e9bcfd

--- public/admin/page_category.php
+++ public/admin/page_category.php
@@ -1,70 +1,66 @@
 <?php
-	$type_id = get_type_id_by_varname($extra);
-	$city_id = false;
+	$category_var_name = $id;
+	$category = get_category_by_var_name($category_var_name);
 	
-	if($type_id && $id != 'all')
+	if ($category)
 	{
-		if ($job->IsValidCategory($id))
+		$category_id = $category['id'];
+		
+		$type_var_name = $extra;
+		$type_id = get_type_id_by_varname($type_var_name);
+		
+		$jobsCount = 0;
+		
+		if ($type_id)
 		{
-			$jobCount =  $job->CountJobs($id, $type_id);
-			$smarty->assign('jobs_count', $jobCount);
+			$jobsCount =  $job->CountJobs($id, $type_id);
 		}
 		else
 		{
-			redirect_to(BASE_URL . 'page-unavailable/');
-			exit;
+			$jobsCount =  $job->CountJobs($id);
 		}
-	}
-	
-	if (!$type_id && $id != 'all')
-	{
-		if ($job->IsValidCategory($id))
-		{
-			$jobCount =  $job->CountJobs($id);
-			$smarty->assign('jobs_count', $jobCount);
-		}
-		else
-		{
-			redirect_to(BASE_URL . 'page-unavailable/');
-			exit;
-		}
-	}
-	else if($id == 'all')
-	{
-		$jobCount =  $job->CountJobs();
-		$smarty->assign('jobs_count', $jobCount);
-	}
-	$paginator = new Paginator($jobCount, JOBS_PER_PAGE, @$_REQUEST['p']);
-	$paginator->setLink(BASE_URL . URL_JOBS . "/$id");
-	$paginator->paginate();
-	
-	$firstLimit = $paginator->getFirstLimit();
-	$lastLimit = $paginator->getLastLimit();
-	
-	$the_jobs = array();
-	$the_jobs = $job->GetJobsPaginate(0, $id, $firstLimit, JOBS_PER_PAGE, 0, 0, false, $city_id, $type_id);
-	
-	$statisticalData = array();
-	
-	if (count($the_jobs))
-	{
-		$jobIDs = array();
 		
-		foreach ($the_jobs as $index => $aJob)
-			$jobIDs[] = $aJob['id'];
+		$paginatorLink = BASE_URL . URL_JOBS . "/$category_var_name";
+	
+		if (isset($type_var_name))
+			$paginatorLink .= "/$type_var_name";
+			
+		$paginator = new Paginator($jobsCount, JOBS_PER_PAGE, @$_REQUEST['p']);
+		$paginator->setLink($paginatorLink);
+		$paginator->paginate();
 		
-		$statisticalData = $job->GetApplicationsStatistics($jobIDs);
+		$firstLimit = $paginator->getFirstLimit();
+		$lastLimit = $paginator->getLastLimit();
+		
+		$the_jobs = $job->GetPaginatedJobsForCategory($category_id, $firstLimit, JOBS_PER_PAGE, $type_id);
+		
+		$statisticalData = array();
+		
+		if (count($the_jobs))
+		{
+			$jobIDs = array();
+			
+			foreach ($the_jobs as $index => $aJob)
+				$jobIDs[] = $aJob['id'];
+			
+			$statisticalData = $job->GetApplicationsStatistics($jobIDs);
+		}
+
+		$smarty->assign("pages", $paginator->pages_link);
+		
+		$smarty->assign('jobs', $the_jobs);
+		$smarty->assign('jobs_count', $jobsCount);
+		$smarty->assign('types', get_types());
+		$smarty->assign('current_category', $category_var_name);
+		$smarty->assign('current_category_name', $category['name']);
+	
+		$template = 'category.tpl';
 	}
-
-	$smarty->assign("pages",$paginator->pages_link);
-	$smarty->assign("statisticalData", $statisticalData);
-
-	$smarty->assign('jobs', $the_jobs);
-	$smarty->assign('current_category', $id);
-	$smarty->assign('current_category_name', get_categ_name_by_varname($id));
-
-	$html_title = 'Are you looking for ' . $extra . ' ' . $id . ' jobs?';
-	$meta_description = '';
+	else
+	{
+		redirect_to(BASE_URL . 'page-unavailable/');
+		exit;
+	}
 
 	$template = 'category.tpl';
 ?>
--- public/page_category.php
+++ public/page_category.php
@@ -1,66 +1,56 @@
 <?php
+	$category_var_name = $id;
+	$category = get_category_by_var_name($category_var_name);
 	
-	$type_id = get_type_id_by_varname($extra);
-	$city_id = false;
-	
-	if($type_id && $id != 'all')
+	if ($category)
 	{
-		if ($job->IsValidCategory($id))
-		{
-			$jobCount =  $job->CountJobs($id, $type_id);
-			$smarty->assign('jobs_count', $jobCount);
-		}
-		else
-		{
-			redirect_to(BASE_URL . 'page-unavailable/');
-			exit;
-		}
-	}
-	
-	if (!$type_id && $id != 'all')
-	{
-		if ($job->IsValidCategory($id))
-		{
-			
-			$jobCount =  $job->CountJobs($id);
-			$smarty->assign('jobs_count', $jobCount);
-		}
-		else
-		{
-			redirect_to(BASE_URL . 'page-unavailable/');
-			exit;
-		}
-	}
-	else if($id == 'all')
-	{
-		$jobCount =  $job->CountJobs();
-		$smarty->assign('jobs_count', $jobCount);
-	}
-	
-	$paginatorLink = BASE_URL . URL_JOBS . "/$id";
-	
-	if (isset($extra))
-		$paginatorLink .= "/$extra";
+		$category_id = $category['id'];
 		
-	$paginator = new Paginator($jobCount, JOBS_PER_PAGE, @$_REQUEST['p']);
-	$paginator->setLink($paginatorLink);
-	$paginator->paginate();
+		$type_var_name = $extra;
+		$type_id = get_type_id_by_varname($type_var_name);
+		
+		$jobsCount = 0;
+		
+		if ($type_id)
+		{
+			$jobsCount =  $job->CountJobs($id, $type_id);
+		}
+		else
+		{
+			$jobsCount =  $job->CountJobs($id);
+		}
+		
+		$paginatorLink = BASE_URL . URL_JOBS . "/$category_var_name";
 	
-	$firstLimit = $paginator->getFirstLimit();
-	$lastLimit = $paginator->getLastLimit();
-	
-	$the_jobs = array();
-	$the_jobs = $job->GetJobsPaginate(0, $id, $firstLimit, JOBS_PER_PAGE, 0, 0, false, $city_id, $type_id);
-	$smarty->assign("pages",$paginator->pages_link);
-	
-	$smarty->assign('jobs', $the_jobs);
-	$smarty->assign('types', get_types());
-	$smarty->assign('current_category', $id);
-	$smarty->assign('current_category_name', get_categ_name_by_varname($id));
+		if (isset($type_var_name))
+			$paginatorLink .= "/$type_var_name";
+			
+		$paginator = new Paginator($jobsCount, JOBS_PER_PAGE, @$_REQUEST['p']);
+		$paginator->setLink($paginatorLink);
+		$paginator->paginate();
+		
+		$firstLimit = $paginator->getFirstLimit();
+		$lastLimit = $paginator->getLastLimit();
+		
+		$the_jobs = $job->GetPaginatedJobsForCategory($category_id, $firstLimit, JOBS_PER_PAGE, $type_id);
 
-	$smarty->assign('seo_title', get_seo_title($id));
-	$smarty->assign('seo_desc', get_seo_desc($id));
-	$smarty->assign('seo_keys', get_seo_keys($id));
-
-	$template = 'category.tpl';
+		$smarty->assign("pages", $paginator->pages_link);
+		
+		$smarty->assign('jobs', $the_jobs);
+		$smarty->assign('jobs_count', $jobsCount);
+		$smarty->assign('types', get_types());
+		$smarty->assign('current_category', $category_var_name);
+		$smarty->assign('current_category_name', $category['name']);
+	
+		$smarty->assign('seo_title', $category['title']);
+		$smarty->assign('seo_desc', $category['description']);
+		$smarty->assign('seo_keys', $category['keywords']);
+	
+		$template = 'category.tpl';
+	}
+	else
+	{
+		redirect_to(BASE_URL . 'page-unavailable/');
+		exit;
+	}
 ?>
