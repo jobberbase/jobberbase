b5b538b2d3836c36b8efbff0a7a3f426be1a8023
filipcte <filipcte@bc0df05e-133d-0410-bb72-81d3b1e9bcfd>
2010-10-19 09:44:02 +0000

    maintenance
    git-svn-id: http://jobberbase.googlecode.com/svn/trunk@473 bc0df05e-133d-0410-bb72-81d3b1e9bcfd

--- public/admin/index.php
+++ public/admin/index.php
@@ -8,12 +8,23 @@
  *             (see license.txt).
  */
 
-	if(!file_exists('config.php')) 
-	{
-	   die('[index.php] config.php not found');
-	}
+	require_once '../_config/config.php';
+	require_once '../_lib/class.Types.php';
 	
-	require_once 'config.php';
+	$currentDirectoryNames = explode('/', dirname($_SERVER['PHP_SELF']));
+	
+	define('CURRENT_DIRECTORY', end($currentDirectoryNames));
+	$smarty->template_dir = APP_PATH . CURRENT_DIRECTORY. '/_tpl/';
+	$smarty->compile_dir = APP_PATH . CURRENT_DIRECTORY. '/_tpl/_cache/';
+
+	define('BASE_URL_ORIG', APP_URL);
+	define('BASE_URL', APP_URL . 'admin/');
+	
+	$page = (isset($_app_info['params'][0]) ? $db->real_escape_string($_app_info['params'][0]) : '');
+	$id = (isset($_app_info['params'][1]) ? $db->real_escape_string($_app_info['params'][1]) : 0);
+	$extra = (isset($_app_info['params'][2]) ? $db->real_escape_string($_app_info['params'][2]) : '');
+	
+	//////////////////////////////////////////////////////////
 	
 	$translator = new Translator(LANG_CODE);
 	$translations = $translator->getTranslations();
