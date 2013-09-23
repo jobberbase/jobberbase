c208119e6167d50d30d058574f28faf83942242f
Rimas Kudelis <rq@akl.lt>
2013-09-23 19:02:38 +0300

    Removing the 'www' directory. It looks like the rename of 'public' to 'www' was committed accidentally. In any case, work on 'www' was abandoned 10 minutes after it was added to the repo, so this directory is just an orphan now.

--- www/admin/page_activate_spotlight.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$j = new Job($_POST['job_id']);
-	$activator = $j->SpotlightActivate();
-	echo 1;
-	exit;
-?>
--- www/admin/page_deactivate_spotlight.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$j = new Job($_POST['job_id']);
-	$deactivator = $j->SpotlightDeactivate();
-	echo 1;
-	exit;
-?>
