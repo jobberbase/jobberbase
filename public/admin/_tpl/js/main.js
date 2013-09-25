dc58982fdc54e15908db5d38ff7fc6a787b3bb14
Rimas Kudelis <rq@akl.lt>
2013-09-25 18:12:15 +0300

    Show statusbar in stripped TinyMCE as well

--- public/admin/_tpl/js/main.js
+++ public/admin/_tpl/js/main.js
@@ -60,7 +60,7 @@
 						theme_advanced_buttons3 : "",
 						theme_advanced_toolbar_location : "top",
 						theme_advanced_toolbar_align : "left",
-						theme_advanced_statusbar_location : full ? "bottom" : "none",
+						theme_advanced_statusbar_location : "bottom",
 						editor_selector : "mceEditor"
 					});
 					this.initialized = true;
