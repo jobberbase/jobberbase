ddbb65db7398dc75a6b4123d2c2d637221193d9f
Rimas Kudelis <rq@akl.lt>
2013-10-11 12:15:55 +0300

    Subsciptions: changes to the Subscriber class.

--- public/_lib/class.Subscriber.php
+++ public/_lib/class.Subscriber.php
@@ -5,123 +5,143 @@
  * @author     Rimas Kudelis <http://rimas.kudelis.lt>
  * @license    You are free to edit and use this work, but it would be nice if you always referenced the original author ;)
  *             (see license.txt).
- * 
- * Subscriptions class handles subscriptions to new job notifications
+ *
+ * Subscriber class – represents a subscriber to job announcements and handles their subscriptions
  */
 
-class Subscriptions extends Postman {
-	public function updateSubscriptions($email, $auth=null, $categories=array())
+class Subscriber {
+	protected $_id;
+	protected $_email;
+	protected $_auth;
+
+	public function __construct($email)
 	{
 		global $db;
 
-		$id = $this->getSubscriberId($email, $auth, $create=true);
-		if ($id)
-		{
-			if (!is_array($categories))
-			{
-				$categories = array($categories);
-			}
-
-			// Delete existing subscriptions
-			$sql = 'DELETE FROM '.DB_PREFIX.'subscriptions WHERE subscriber_id = '.$id;
-			if ($db->Execute($sql))
-			{
-				if (empty($categories))
-				{
-					// Do nothing – updating to "no subscriptions"
-					return true;
-				}
-				else
-				{
-					// Add the supplied subscriptions
-					$addCombos = array();
-					foreach ($categories as $category)
-					{
-						$addCombos = '('.$email.','.$category.')';
-					}
-					$addCombos = implode(',', $addCombos);
-					$sql = 'INSERT INTO '.DB_PREFIX.'subscriptions (subscriber_id, category_id) VALUES '.$addCombos;
-					return $db->Execute($sql);
-				}
-			}
-		}
-		return false;
-	}
-
-	public function unsubscribeAll($email, $auth=null)
-	{
-		global $db;
-
-		$id = $this->getSubscriberId($email, $auth);
-		if ($id)
-		{
-			$sql = 'DELETE FROM '.DB_PREFIX.'subscriptions WHERE subscriber_id = ' . $id . ';'
-			     . 'DELETE FROM '.DB_PREFIX.'subscribers WHERE id = ' . $id;
-			return $db->ExecuteMultiple($sql);
-		}
-		return false;
-	}
-
-	public function getSubscriptions($email, $auth=null)
-	{
-		global $db;
-
-		$id = $this->getSubscriberId($email, $auth);
-		if ($id)
-		{
-			$sql = 'SELECT category_id FROM '.DB_PREFIX.'subscriptions WHERE subscribed_id = ' . $id;
-			if ($tmpResult = $db->QueryArray($sql))
-			{
-				$result = array_map(function($item){ return $item['category_id']; }, $tmpResult);
-				return $result;
-			}
-		}
-		return false;
-	}
-
-	public function confirmSubscriber($email, $auth=null)
-	{
-		global $db;
-
-		if (is_null($auth))
-		{
-			$sql = 'UPDATE '.DB_PREFIX.'subscribers SET confirmed = 1 WHERE email = "' . $email . '"';
-		}
-		else
-		{
-			$sql = 'UPDATE '.DB_PREFIX.'subscribers SET confirmed = 1 WHERE email = "' . $email . '" AND auth = "' . $auth .'"';
-		}
-		$db->Execute($sql);
-		return (bool) $db->affected_rows;
-	}
-
-	protected function getSubscriberId($email, $auth=null, $create=false)
-	{
-		global $db;
+		$email = (string)$email;
 
 		$sql = 'SELECT id, auth FROM '.DB_PREFIX.'subscribers WHERE email = "' . $email . '"';
 		$result = $db->QueryRow($sql);
 		if ($result)
 		{
-			if ($result['auth'] == $auth)
-			{
-				return $id;
-			}
+			$this->_email = $email;
+			$this->_id = $result['id'];
+			$this->_auth = $result['auth'];
 		}
-		elseif ($create)
+		else
 		{
-			$auth = $this->generateAuthCode();
-			$sql = 'INSERT INTO '.DB_PREFIX.'subscribers (email, auth, confirmed)
-						   VALUES ("' . $email . '", "' . $auth . '", 0)';
+			$auth = self::generateAuthCode();
+			$sql = 'INSERT INTO '.DB_PREFIX.'subscribers (email, auth)
+			               VALUES ("' . $email . '", "' . $auth . '")';
 			if($db->Execute($sql))
 			{
-				return $db->insert_id;
+				$this->_email = $email;
+				$this->_id = $db->insert_id;
+				$this->_auth = $auth;
 			}
+		}
+	}
+
+	public function getAuthCode()
+	{
+		return $this->_auth;
+	}
+
+	// Update/confirm the list of subscriptions for the user.
+	public function updateSubscriptions($categories=array())
+	{
+		global $db;
+
+		if (!is_array($categories))
+		{
+			$categories = array($categories);
+		}
+		// Delete existing subscriptions
+		$sql = 'DELETE FROM '.DB_PREFIX.'subscriptions WHERE subscriber_id = ' . $this->_id;
+		if ($db->Execute($sql))
+		{
+			if (empty($categories))
+			{
+				// Do nothing – updating to "no subscriptions"
+				return true;
+			}
+			else
+			{
+				// Add the supplied subscriptions
+				$addCombos = array();
+				foreach ($categories as $category)
+				{
+					$addCombos[] = '(' . $this->_id . ',' . (int)$category . ',1)';
+				}
+				$addCombos = implode(',', $addCombos);
+				$sql = 'INSERT INTO '.DB_PREFIX.'subscriptions (subscriber_id, category_id, confirmed) VALUES ' . $addCombos;
+				return $db->Execute($sql);
+			}
+		}
+	}
+
+	// Subscribe an email to a given category
+	public function addSubscription($category, $confirmed=false)
+	{
+		global $db;
+
+		if ($this->isSubscribed($category))
+		{
+			return true; // do nothing if already subscribed and confirmed
+		}
+
+		$sql = 'REPLACE INTO '.DB_PREFIX.'subscriptions (subscriber_id, category_id, confirmed) VALUES (' . $this->_id . ',' . (int)$category . ',' . (int)(bool)$confirmed . ')';
+		return $db->Execute($sql);
+	}
+
+	public function getConfirmedSubscriptions()
+	{
+		return $this->getSubscriptions(true);
+	}
+
+	public function getUnconfirmedSubscriptions()
+	{
+		return $this->getSubscriptions(false);
+	}
+
+	protected function getSubscriptions($confirmed=true)
+	{
+		global $db;
+
+		$sql = 'SELECT category_id FROM '.DB_PREFIX.'subscriptions WHERE subscriber_id = ' . $this->_id . ' AND confirmed = ' . (int)(bool)$confirmed;
+		if ($tmpResult = $db->QueryArray($sql))
+		{
+			$result = array_map(function($item){ return $item['category_id']; }, $tmpResult);
+			return $result;
 		}
 		return false;
 	}
 
-	protected function generateAuthCode()
+	// Returns true if a given user is subscribed to a given category and the subscription is confirmed
+	public function isSubscribed($category)
+	{
+		global $db;
+
+		$sql = 'SELECT confirmed FROM '.DB_PREFIX.'subscriptions WHERE subscriber_id = ' . $this->_id . ' AND category_id = ' . (int)$category;
+		return (bool)$db->QueryItem($sql);
+	}
+
+	public function delete()
+	{
+		global $db;
+
+		$sql = 'DELETE FROM '.DB_PREFIX.'subscriptions WHERE subscriber_id = ' . $this->_id . ';'
+			 . 'DELETE FROM '.DB_PREFIX.'subscribers WHERE id = ' . $this->_id;
+		return $db->ExecuteMultiple($sql);
+	}
+
+	public static function isValid($email, $auth)
+	{
+		$sql = 'SELECT id FROM '.DB_PREFIX.'subscribers WHERE email = ' . $email . ' AND auth = ' . $auth;
+		return (bool)$db->QueryItem($sql);
+	}
+
+	protected static function generateAuthCode()
 	{
 		$auth = md5(uniqid() . time());
 		return $auth;
