722aff896a8817f4ea85237bd67745610a90c224
Rimas Kudelis <rq@akl.lt>
2013-12-15 12:15:43 +0200

    FORUM_URL/subscribe/ will now show a generic subscribe form (which subscribes to all cactegories) by default. It's possible to add some introductory text to this page.

--- public/page_subscribe.php
+++ public/page_subscribe.php
@@ -1,30 +1,56 @@
 <?php
-	if (!empty($_POST) && isset($_POST['email']) && isset($_POST['category']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
+	if (!empty($_POST) && isset($_POST['email']) && isset($_POST['category']))
 	{
-		$email = $db->real_escape_string($_POST['email']);
-		$category = $db->real_escape_string($_POST['category']);
+		if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+			$email = $db->real_escape_string($_POST['email']);
+			$category = $db->real_escape_string($_POST['category']);
 
-		if ($category != '0')
-		{
-			$categoryData = get_category_by_id($category);
-			if (is_null($categoryData)) // category does not exist
+			if ($category != '0')
 			{
-				echo '0';
-				exit;
+				$categoryData = get_category_by_id($category);
+				if (is_null($categoryData)) // category does not exist
+				{
+					echo '0';
+					exit;
+				}
+			}
+
+			$subscriber = new Subscriber($email);
+			$postman = new Postman();
+			if ($subscriber->addSubscription($category))
+			{
+				if ($postman->MailSubscriptionPleaseConfirm($email, $subscriber->getAuthCode()))
+				{
+					echo '1';
+					exit;
+				}
 			}
 		}
-
-		$subscriber = new Subscriber($email);
-		$postman = new Postman();
-		if ($subscriber->addSubscription($category))
-		{
-			if ($postman->MailSubscriptionPleaseConfirm($email, $subscriber->getAuthCode()))
-			{
-				echo '1';
-				exit;
-			}
-		}
+		echo '0';
+		exit;
 	}
-	echo '0';
-	exit;
+	else // present a subscribe form with introductory text, if set
+	{
+		if ($settings['subscribe_page_content'] != '')
+		{
+			$sql = '
+				SELECT *
+				FROM '.DB_PREFIX.'pages 
+				WHERE url = "' . $db->real_escape_string($settings['subscribe_page_content']) . '"';
+			$result = $db->query($sql);
+			$pageData = $result->fetch_assoc();
+			if (is_array($pageData)) {
+				$html_title = $pageData['page_title'] . ' - ' . SITE_NAME;
+				$meta_keywords = $pageData['keywords'];
+				$meta_description = $pageData['description'];
+				$smarty->assign('page', $pageData);
+			}
+		}
+
+		$smarty->assign('seo_title', htmlspecialchars($settings['html_title']));
+		$smarty->assign('seo_desc', htmlspecialchars($settings['meta_description']));
+		$smarty->assign('seo_keys', htmlspecialchars($settings['meta_keywords']));
+
+		$template = 'subscribe.tpl';
+	}
 ?>
