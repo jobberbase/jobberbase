a8c247d7a666e9c4064e45fad003ed7e6172b10a
Rimas Kudelis <rq@akl.lt>
2013-12-26 11:53:05 +0200

    Improved the translation import script: * its configuration has been moved to the top * it now gives visual feedback * it now creates an entry in i18n_langs as well as entries in i18n_translations, if needed * it now fails if in the DB it finds any strings for the language that is being imported

--- public/_tools/translations/translations_in_db.php
+++ public/_tools/translations/translations_in_db.php
@@ -7,114 +7,143 @@
  *             (see license.txt).
  */
 
+	/***  CONFIGURATION STARTS HERE  ***/
+	$langName = 'English';
+	$langCode = 'en';
+	die('This script is disabled for security purposes!<br />Comment out or delete this line (it lives in the configuration section of this file and starts with word "die") to use it.');
+	/***  CONFIGURATION ENDS HERE  ***/
+
 	require_once '../../_config/config.php';
 	
 	$cache->removeCache(CACHE_TRANSLATIONS);
-	
-	$lang_id = 1;
-	
-	
-	// strings / .ini
-	if (0)
+
+	// test if language is already created in the database
+	$sql = "SELECT id FROM i18n_langs WHERE code='{$langCode}'";
+	$lang_id = $db->queryItem($sql);
+	if ($lang_id)
 	{
-		$translationFilePath = APP_PATH . '_tools/translations/src/en/translations_en.ini';
-
-		if (file_exists($translationFilePath)) 
-		{
-			$translations = parse_ini_file($translationFilePath, true);
-		}
-
-		foreach ($translations as $t_section => $t_data)
-		{
-			$sql = 'INSERT INTO i18n_translations (id, parent_id, lang_id, item) VALUES (NULL, 0, ' . $lang_id . ', "' . $t_section . '")';
-			$db->Execute($sql);
-
-			$parent_id = $db->insert_id;
-
-			foreach ($t_data as $k => $v)
-			{
-				$sql = 'INSERT INTO i18n_translations (parent_id, lang_id, item, value) VALUES (' . $parent_id . ', ' . $lang_id . ', "' . addslashes($k) . '", "' . addslashes($v) . '")';
-				$db->Execute($sql);
-			}
-		}	
+		echo 'Existing entry found in i18n_langs.<br />';
+	}
+	else
+	{
+		echo 'Creating an entry in i18n_langs...<br />';
+		// if the language is not created yet, create it.
+		$sql = "INSERT INTO i18n_langs (name, code) VALUES ('{$langName}', '{$langCode}')";
+		$db->Execute($sql);
+		$lang_id = $db->insert_id;
 	}
 	
-	
-	
-	// emails / .xml
-	if (0)
+	if ($lang_id)
 	{
-		$translationFilePath = APP_PATH . '_tools/translations/src/en/emails_en.xml';
+		$sql = "SELECT COUNT(id) FROM i18n_translations WHERE lang_id={$lang_id}";
+		$count = $db->queryItem($sql);
+		if ($count)
+		{
+			die('Some entries for this language id already exist in the database!<br />Please remove them manually or truncate the i18n_translations table before importing again.');
+		}
+
+		echo 'No strings have been found for this language id in the database. Continuing with import...<br />';
+		// strings / .ini
+		$translationFilePath = APP_PATH . "_tools/translations/src/{$langCode}/translations_{$langCode}.ini";
 
 		if (file_exists($translationFilePath)) 
 		{
-			$xml = simplexml_load_file($translationFilePath);
-		}
-		
-		foreach ($xml as $email => $fields)
-		{
-			$item = (string) $email;
-			$message = str_replace('{BR}', "\n", (string) $fields->message);
-			
-			
-			if ($item == 'PublishToAdmin')
+			echo 'UI translation file found. Importing...<br />';
+			$translations = parse_ini_file($translationFilePath, true);
+
+			foreach ($translations as $t_section => $t_data)
 			{
-				$subject = (string) $fields->newPostSubject;
-				
-				// 1st email: first post by a user
-				$msg1 = str_replace('{BR}', "\n", (string) $fields->messageFirstPost) . $message;
-				
-				// create new section for this email
-				$item1 = 'email_' . $item . '_firstPost';
-				$sql = 'INSERT INTO i18n_translations (id, parent_id, lang_id, item) VALUES (NULL, 0, ' . $lang_id . ', "' . $item1 . '")';
-				$db->Execute($sql);
-				$parent_id1 = $db->insert_id;
-
-				// add subject as translation item
-				$sql = 'INSERT INTO i18n_translations (parent_id, lang_id, item, value) VALUES (' . $parent_id1 . ', ' . $lang_id . ', "subject", "' . addslashes(trim($subject)) . '")';
+				$sql = "INSERT INTO i18n_translations (id, parent_id, lang_id, item) VALUES (NULL, 0, {$lang_id}, '{$t_section}')";
 				$db->Execute($sql);
 
-				// add message as translation item
-				$sql = 'INSERT INTO i18n_translations (parent_id, lang_id, item, value, field_type) VALUES (' . $parent_id1 . ', ' . $lang_id . ', "message", "' . addslashes(trim($msg1)) . '", "textarea")';
-				$db->Execute($sql);
-				
-				
-				
-				// 2nd email: usual email
-				$msg2 = $message;
-				
-				// create new section for this email
-				$item2 = 'email_' . $item;
-				$sql = 'INSERT INTO i18n_translations (id, parent_id, lang_id, item) VALUES (NULL, 0, ' . $lang_id . ', "' . $item2 . '")';
-				$db->Execute($sql);
-				$parent_id2 = $db->insert_id;
-
-				// add subject as translation item
-				$sql = 'INSERT INTO i18n_translations (parent_id, lang_id, item, value) VALUES (' . $parent_id2 . ', ' . $lang_id . ', "subject", "' . addslashes(trim($subject)) . '")';
-				$db->Execute($sql);
-
-				// add message as translation item
-				$sql = 'INSERT INTO i18n_translations (parent_id, lang_id, item, value, field_type) VALUES (' . $parent_id2 . ', ' . $lang_id . ', "message", "' . addslashes(trim($msg2)) . '", "textarea")';
-				$db->Execute($sql);
-			}
-			else
-			{
-				$subject = (string) $fields->subject;
-				
-				// create new section for this email
-				$item = 'email_' . $item;
-				$sql = 'INSERT INTO i18n_translations (id, parent_id, lang_id, item) VALUES (NULL, 0, ' . $lang_id . ', "' . $item . '")';
-				$db->Execute($sql);
 				$parent_id = $db->insert_id;
 
-				// add subject as translation item
-				$sql = 'INSERT INTO i18n_translations (parent_id, lang_id, item, value) VALUES (' . $parent_id . ', ' . $lang_id . ', "subject", "' . addslashes(trim($subject)) . '")';
-				$db->Execute($sql);
-
-				// add message as translation item
-				$sql = 'INSERT INTO i18n_translations (parent_id, lang_id, item, value, field_type) VALUES (' . $parent_id . ', ' . $lang_id . ', "message", "' . addslashes(trim($message)) . '", "textarea")';
-				$db->Execute($sql);
-				
+				foreach ($t_data as $k => $v)
+				{
+					$sql = "INSERT INTO i18n_translations (parent_id, lang_id, item, value) VALUES ({$parent_id}, {$lang_id}, '" . addslashes($k) . "', '" . addslashes($v) . "')";
+					$db->Execute($sql);
+				}
 			}
 		}
+		else
+		{
+			echo 'UI translation file not found! Website translations will not be available for this language!<br />';
+		}
+
+		// emails / .xml
+		$translationFilePath = APP_PATH . "_tools/translations/src/{$langCode}/emails_{$langCode}.xml";
+
+		if (file_exists($translationFilePath)) 
+		{
+			echo 'Email translation file found. Importing...<br />';
+			$xml = simplexml_load_file($translationFilePath);
+
+			foreach ($xml as $email => $fields)
+			{
+				$item = (string) $email;
+				$message = str_replace('{BR}', "\n", (string) $fields->message);
+
+				if ($item == 'PublishToAdmin')
+				{
+					$subject = (string) $fields->newPostSubject;
+
+					// 1st email: first post by a user
+					$msg1 = str_replace('{BR}', "\n", (string) $fields->messageFirstPost) . $message;
+
+					// create new section for this email
+					$item1 = 'email_' . $item . '_firstPost';
+					$sql = 'INSERT INTO i18n_translations (id, parent_id, lang_id, item) VALUES (NULL, 0, ' . $lang_id . ', "' . $item1 . '")';
+					$db->Execute($sql);
+					$parent_id1 = $db->insert_id;
+
+					// add subject as translation item
+					$sql = 'INSERT INTO i18n_translations (parent_id, lang_id, item, value) VALUES (' . $parent_id1 . ', ' . $lang_id . ', "subject", "' . addslashes(trim($subject)) . '")';
+					$db->Execute($sql);
+
+					// add message as translation item
+					$sql = 'INSERT INTO i18n_translations (parent_id, lang_id, item, value, field_type) VALUES (' . $parent_id1 . ', ' . $lang_id . ', "message", "' . addslashes(trim($msg1)) . '", "textarea")';
+					$db->Execute($sql);
+
+					// 2nd email: usual email
+					$msg2 = $message;
+
+					// create new section for this email
+					$item2 = 'email_' . $item;
+					$sql = 'INSERT INTO i18n_translations (id, parent_id, lang_id, item) VALUES (NULL, 0, ' . $lang_id . ', "' . $item2 . '")';
+					$db->Execute($sql);
+					$parent_id2 = $db->insert_id;
+
+					// add subject as translation item
+					$sql = 'INSERT INTO i18n_translations (parent_id, lang_id, item, value) VALUES (' . $parent_id2 . ', ' . $lang_id . ', "subject", "' . addslashes(trim($subject)) . '")';
+					$db->Execute($sql);
+
+					// add message as translation item
+					$sql = 'INSERT INTO i18n_translations (parent_id, lang_id, item, value, field_type) VALUES (' . $parent_id2 . ', ' . $lang_id . ', "message", "' . addslashes(trim($msg2)) . '", "textarea")';
+					$db->Execute($sql);
+				}
+				else
+				{
+					$subject = (string) $fields->subject;
+
+					// create new section for this email
+					$item = 'email_' . $item;
+					$sql = 'INSERT INTO i18n_translations (id, parent_id, lang_id, item) VALUES (NULL, 0, ' . $lang_id . ', "' . $item . '")';
+					$db->Execute($sql);
+					$parent_id = $db->insert_id;
+
+					// add subject as translation item
+					$sql = 'INSERT INTO i18n_translations (parent_id, lang_id, item, value) VALUES (' . $parent_id . ', ' . $lang_id . ', "subject", "' . addslashes(trim($subject)) . '")';
+					$db->Execute($sql);
+
+					// add message as translation item
+					$sql = 'INSERT INTO i18n_translations (parent_id, lang_id, item, value, field_type) VALUES (' . $parent_id . ', ' . $lang_id . ', "message", "' . addslashes(trim($message)) . '", "textarea")';
+					$db->Execute($sql);
+				}
+			}
+		}
+		else
+		{
+			echo 'Email translation file not found! Website translations will not be available for this language!<br />';
+		}
+		echo 'Done!';
 	}
